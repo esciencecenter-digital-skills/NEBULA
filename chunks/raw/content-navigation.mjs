64e825ad433f90ccdfcba62382563acb4487142d
bvreede <bvreede@users.noreply.github.com>
2023-07-24 12:59:44 +0000

    deploy: 43ae61551c813690cc8387cfc21638d288bc087f

--- chunks/raw/content-navigation.mjs
+++
@@ -1,5 +0,0 @@
-// ROLLUP_NO_REPLACE 
- const contentNavigation = "[{\"title\":\"Hello world\",\"_path\":\"/\"},{\"title\":\"Lessons\",\"_path\":\"/lessons\",\"children\":[{\"title\":\"Demo lesson\",\"_path\":\"/lessons/demo\",\"children\":[{\"title\":\"Exercise\",\"_path\":\"/lessons/demo/exercise\"},{\"title\":\"Presentation\",\"_path\":\"/lessons/demo/presentation\"},{\"title\":\"Reading material\",\"_path\":\"/lessons/demo/reading\"}]}]}]";
-
-export { contentNavigation as default };
-//# sourceMappingURL=content-navigation.mjs.map
