022e5f998956bd7d2f78076791d772131dab6071
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-07-24 11:14:41 +0200

    set SSR to false to enable SPA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true,
-              pages: true},
+              pages: true },
   modules: [
     '@nuxt/content'
   ],
   content: {
     // https://content.nuxtjs.org/api/configuration
-  }
+  },
+  ssr: false
 })
