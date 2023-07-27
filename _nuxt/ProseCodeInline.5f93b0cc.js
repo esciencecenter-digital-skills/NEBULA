f5332a46fd395ada303eb88cd5039add6aee6535
bvreede <bvreede@users.noreply.github.com>
2023-07-27 09:32:44 +0000

    deploy: 8862d6ceb68ed7d1522f43dcd87ab9f87e7422ef

---
+++ _nuxt/ProseCodeInline.5f93b0cc.js
@@ -0,0 +1,1 @@
+import{_ as o,o as n,c as r,M as c}from"./entry.31c0e71e.js";const s={};function t(e,a){return n(),r("code",null,[c(e.$slots,"default")])}const _=o(s,[["render",t]]);export{_ as default};
