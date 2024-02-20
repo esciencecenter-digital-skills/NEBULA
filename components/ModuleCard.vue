4a4fea89282af47b1844d00f9ef701627a830149
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2024-02-20 14:02:04 +0100

    Add storycards and loop in index

---
+++ components/ModuleCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink :to="url">
+    <!-- relative parent needed for absolute positioning of svg badges-->
+    <div class="relative">
+      <div class="flex flex-row bg-eScienceWhite shadow-xl max-w-xl rounded-bl-3xl rounded-tr-3xl h-48">
+        <!-- Module cards -->
+        <div class="prose font-display m-4 mr-10">
+          <h3>
+            {{ title }}
+          </h3>
+        </div>
+        <img :src="thumbnail" alt="module icon" class="max-w-xs rounded-tr-3xl">
+      </div>
+    </div>
+  </NuxtLink>
+</template>
+
+<script>
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    author: {
+      type: String,
+      default: 'Author'
+    },
+    thumbnail: {
+      type: String,
+      default: './static/nlesc-logo.svg'
+    },
+    url: {
+      type: String,
+      default: '/'
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,63 @@
 <template>
-    <main>
-        <Banner />
-        <ContentDoc />
-        Testing import from the config file: {{ $config.public.another }}
-    </main>
-</template>
-<script>
-</script>
+    <div class="flex flex-col w-screen bg-gentleBlue h-screen overflow-auto">
+      <Banner />
+      <input v-model="query" type="search" class="w-2/3 mt-4 mr-4 p-2 self-end" placeholder="search">
+      <div class="flex flex-col pt-4 pb-6 pl-6">
+        <!-- categories -->
+        <div v-for='category in ["Getting started", "Reusability", "Publishing & Citing", "Resources", "Examples", "Upcoming (under construction)"]' :key="category" class="flex flex-wrap gap-4 mb-8">
+          <h2 class="prose-2xl font-display font-bold text-eSciencePurple w-full pl-2">
+            {{ category }}
+          </h2>
+          <!-- stories -->
+          <StoryCard
+            v-for="story in [{'id': 4, 'slug': 'yolo', 'title': 'yolo', 'author': '', 'thumbnail': '', 'category': 'Examples', 'visibility': True}]"
+            :key="story.id"
+            :title="story.title"
+            :author="story.author"
+            :thumbnail="`modules/_${story.slug}/${story.thumbnail}`"
+            :url="story.slug"
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  </template>
+  
+  <script>
+//   import '../layouts/style.scss'
+  
+//   export default {
+//     async asyncData (context) {
+//       const stories = await context.$content()
+//         .only(['id', 'slug', 'title', 'author', 'thumbnail', 'category', 'visibility'])
+//         .fetch()
+//         .catch(e => console.log(e))
+  
+//       const categories = stories
+//         .filter(story => story.visibility === 'visible')
+//         .map(story => story.category)
+//         .filter((v, i, a) => a.indexOf(v) === i)
+//         .sort((a, b) => {
+//           return context.$config.categoryOrder.indexOf(a) - context.$config.categoryOrder.indexOf(b)
+//         })
+  
+//       return { stories, categories }
+//     },
+//     data () {
+//       return {
+//         query: ''
+//       }
+//     },
+//     watch: {
+//       async query (query) {
+//         this.stories = await this.$content()
+//           .only(['id', 'slug', 'title', 'author', 'thumbnail', 'category'])
+//           .search(query)
+//           .fetch()
+//           .catch(e => console.log(e))
+//       }
+//     }
+//   }
+
+    
+  </script>
