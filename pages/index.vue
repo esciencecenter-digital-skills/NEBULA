492544026806849120a2f3417bf48bc26cfe127d
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2024-02-21 13:38:54 +0100

    change thumbnail location

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
             :key="story.id"
             :title="story.title"
             :author="story.author"
-            :thumbnail="`/modules/${story.slug}/_assets/${story.thumbnail}`"
+            :thumbnail="`modules/${story.slug}/_assets/${story.thumbnail}`"
             :url="`/modules/${story.slug}`"
             />
         </div>
