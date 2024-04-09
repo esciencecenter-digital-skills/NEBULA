aa2a122ea20dfcf5eda32fe02a69d158a574b2c0
raar1 <raarichardson@gmail.com>
2024-04-09 10:59:11 +0200

    Use .pmd as extension for denoting reveal presentations

--- pages/modules/[module]/[chapter].vue
+++ pages/modules/[module]/[chapter].vue
@@ -3,7 +3,7 @@
       <div class="flex flex-auto overflow-auto p-4 bg-gentleBlue font-body h-full w-full">
         <div class="flex-initial bg-white p-4 w-1/3 rounded overflow-auto">
             <ContentDoc v-slot="{ doc }">
-                <div v-if="doc._extension==='presmd'">
+                <div v-if="doc._extension==='pmd'">
                     <Slides :slidescontent="doc._file"/>
                 </div>
                 <div v-else>
