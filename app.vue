76b92c12baa3a4f8f2a023ba6af4cf83a4e98b23
Djura Smits <djura.smits@gmail.com>
2024-04-10 09:13:19 +0200

    Removed some stuff

--- app.vue
+++ app.vue
@@ -2,9 +2,8 @@
 
     <div class="flex flex-col w-screen bg-gentleBlue h-screen overflow-auto">
       <Banner />
-      div flex-1
+
       <NuxtPage />
-      /div
       <Footer />
     </div>
 </template>
