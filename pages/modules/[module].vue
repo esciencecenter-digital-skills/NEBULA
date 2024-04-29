dd3e1da9ea886bf5b8738e08f4d99779d752a936
Cmurilochem <carlosmurilorocha@gmail.com>
2024-04-29 15:21:52 +0200

    Replace changes by a simple ContentDoc

--- pages/modules/[module].vue
+++ pages/modules/[module].vue
@@ -1,21 +1,11 @@
 <template>
     <div class="flex flex-col h-full w-full ">
 
-      <!-- Print name of the module by searching for its *.pm file and `title` field -->
-      <div class="content-container">
-        <ContentList path="/modules" :query="modQuery" v-slot="{ list }">
-          <!-- Iterate through the list to find matching modObject -->
-          <div v-for="modObject in list.filter(mo => mo._path === $route.path)" :key="modObject.id">
-            <h2 class="prose-2xl font-display font-bold text-eSciencePurple w-full pl-2">
-              Module {{ modObject.title }}
-            </h2>
-          </div>
-          <!-- Just in case: display a fallback message if the list is empty or no match -->
-          <div v-if="list.length === 0 || list.every(mo => mo._path !== $route.path)">
-            <p>No module found for this path.</p>
-          </div>
-        </ContentList>
-      </div>
+      <ContentDoc v-slot="{ doc }">
+        <h2 class="prose-2xl font-display font-bold text-eSciencePurple w-full pl-2">
+          Module {{ doc.title }}
+        </h2>
+      </ContentDoc>
 
       <div class="flex no-wrap text-left">
         <ContentList :path="'/modules/' + $route.params.module + '/'" v-slot="{ list }">
@@ -34,14 +24,3 @@
       <NuxtPage />
     </div>
 </template>
-
-<!-- Query visible modules only -->
-<script setup lang="ts">
-
-    import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
-
-    const runtimeConfig = useRuntimeConfig()
-
-    const modQuery: QueryBuilderParams = { path: '/modules', where: [{ visibility: 'visible' }] }
-
-</script>
