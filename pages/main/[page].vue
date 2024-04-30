5a9710b683a2f47f1a540d48d73027ef57d71724
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2024-04-30 09:07:25 +0200

    move main level pages to subfolder main

---
+++ pages/main/[page].vue
@@ -0,0 +1,7 @@
+<template>
+  <ContentDoc v-slot="{ doc }">
+    <div class="flex font-body m-10 mx-60 bg-eScienceWhite justify-center py-8">
+      <ContentRenderer :value="doc" class="prose-lg max-w-2xl" />
+    </div>
+  </ContentDoc>
+</template>
