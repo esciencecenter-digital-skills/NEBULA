04c83b0dfff951d7bfebea8031e0bb3a91661cd4
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2024-05-01 16:25:05 +0200

    clean up query, point card to info page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class="flex flex-col pt-4 pb-6 pl-6">
+    <div class="flex flex-col pt-4 pb-6 pl-6 ml-16">
             <!-- categories -->
-            <div v-for='category in runtimeConfig.public.categoryOrder' :key="category" class="flex flex-wrap gap-4 mb-8">
+            <div v-for='category in config.categoryOrder' :key="category" class="flex flex-wrap gap-4 mb-8">
                 <h2 class="prose-2xl font-display font-bold text-eSciencePurple w-full pl-2">
                 {{ category }}
                 </h2>
                 <!-- modules -->
-                <ContentList path="/modules" :query="modQuery" v-slot="{ list }">
-                    <ModuleCard v-for="modObject in list
-                            .filter(modObject => (modObject.category === category && modObject.visibility === 'visible'))
-                            .sort((a, b) => a.id - b.id)"
+                <ContentList :query="modQuery" v-slot="{ list }">
+                    <ModuleCard v-for="modObject in list.filter(modObject => (modObject.category === category))"
                         :key="modObject.id"
                         :title="modObject.title"
                         :author="modObject.author"
-                        :thumbnail="`/${runtimeConfig.public.repoName}/${modObject._path}/media/${modObject.thumbnail}`"
-                        :url="modObject._path"
+                        :thumbnail="`/${config.repoName}${modObject._path}/media/${modObject.thumbnail}`"
+                        :url="`${modObject._path}/info`"
                     />
                 </ContentList>
             </div>
@@ -25,27 +23,13 @@
 
     import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
     
-    const runtimeConfig = useRuntimeConfig()
+    const config = useRuntimeConfig().public
+   
+    const modQuery: QueryBuilderParams = { 
+        path: '/modules', 
+        where: [{ visibility: 'visible' }], 
+        sort: [{ id:1, $numeric:true }] 
+    }
 
-    const {
-        // Global references
-        globals,
-        navigation,
-        surround,
-        page,
-        // Computed properties from `page` key
-        excerpt,
-        toc,
-        type,
-        layout,
-        // Computed properties from `surround` key
-        next,
-        prev
-    } = useContent()
-
-    console.log(globals);
-    
-    const modQuery: QueryBuilderParams = { path: '/modules', where: [{ visibility: 'visible' }] }
-
-           
+          
 </script>
