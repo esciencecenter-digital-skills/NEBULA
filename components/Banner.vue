5e9ed463bf026a02d1edd55af0a12c243682be68
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2024-05-01 16:23:06 +0200

    add logo to Banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -2,7 +2,7 @@
     <div class="flex p-5 bg-eScienceWhite text-xl items-center text-eSciencePurple">
         <div class="flex flex-1 gap-10 items-center ">
           <NuxtLink :to="`/`">
-            <div class="text-4xl font-bold">
+            <div class="text-4xl mx-16 font-bold">
               {{ $config.public.title }}
             </div>
           </NuxtLink>
@@ -11,20 +11,23 @@
         <div class="flex align-middle items-center">
           <div class="p-5">
             
-            <NuxtLink :to="`/`" class="p-5">
+            <NuxtLink :to="`/`" class="p-5 hover:text-eScienceBlue">
               Modules
             </NuxtLink>
 
             <ContentList :path="'/main'"  v-slot="{ list }">
-              <nuxt-link v-for="page in list" :key="page._path" :to="page._path" class="p-5">
+              <nuxt-link v-for="page in list" :key="page._path" :to="page._path" class="p-5 hover:text-eScienceBlue">
                     {{ page.title }}
               </nuxt-link>
             </ContentList>
             
           </div>
-            <NuxtLink to="https://www.github.com/esciencecenter-digital-skills/research-software-support" target="_blank" class=" pl-10">
-              <img src="~/public/github-mark.svg" alt="Netherlands eScience Center Logo" width="50px" height="50px" class="align-middle">
-            </NuxtLink>
+          <NuxtLink to="https://www.github.com/esciencecenter-digital-skills/research-software-support" target="_blank" class=" pl-10">
+            <img src="/icons/github-mark.svg" alt="Netherlands eScience Center Logo" width="50px" height="50px" class="align-middle">
+          </NuxtLink>
+          <NuxtLink to="https://www.esciencecenter.nl" target="_blank" class="px-5">
+            <img src="/nlesc-logo.svg" alt="Netherlands eScience Center Logo" width="200px">
+          </NuxtLink>
         </div>
 
     </div>
--- components/Footer.vue
+++ components/Footer.vue
@@ -5,11 +5,7 @@
       <div class="text-center">
         These materials are developed by the <a href="https://www.esciencecenter.nl">Netherlands eScience Center</a>. Licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a> unless otherwise noted.
       </div>
-      <!-- <div class="bg-gentleBlue ps-4 justify-right">
-        <NuxtLink to="https://www.esciencecenter.nl" target="_blank" class="p-5">
-          <img src="~/public/nlesc-logo.svg" alt="Netherlands eScience Center Logo" width="100px">
-        </NuxtLink>
-      </div> -->
+
     </div>
   </div>
 </template>
