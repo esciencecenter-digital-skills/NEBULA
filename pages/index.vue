3d064d590a4c6b41afcd187cab49603db6ed09a7
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2025-01-21 13:42:59 +0100

    use default root directory instead of config specified baseURL for module thumbnails

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
                 :title="modObject.title"
                 :author="modObject.author"
                 :abstract="modObject.abstract"
-                :thumbnail="`/${config.baseURL}/${modObject._path}/media/${modObject.thumbnail}`"
+                :thumbnail="`${modObject._path}/media/${modObject.thumbnail}`"
                 :url="`${modObject._path}/info`"
                 class="z-10"/>
             </ContentList>
@@ -32,5 +32,4 @@
         where: [{ visibility: 'visible' }], 
         sort: [{ order:1, $numeric:true }] 
     }
-         
 </script>
